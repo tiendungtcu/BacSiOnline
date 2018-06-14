aaad37e22eb7770d27e0054a1cf8081b02d86126
Dung Nguyen <encorageugly@gmail.com>
2018-06-14 15:23:36 +0700

    Add AccountController.cs file

---
+++ AccountController.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using UserRoleMan.Models;
+
+namespace UserRoleMan.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+        private ApplicationDbContext context;
+        private BaoBanEntities db;
+
+        public AccountController()
+        {
+            context = new ApplicationDbContext();
+            db = new BaoBanEntities();
+        }
+
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set 
+            { 
+                _signInManager = value; 
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        //
+        // GET: /Account/Login
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                int id = User.Identity.GetUserId<int>();
+                // nếu là trực ban đơn vị
+                if (UserManager.IsInRole(id, "DonVi"))
+                {
+                    return RedirectToAction("Index", "BaoBanQs");
+                }
+                // nếu là Quan trị viên
+                else if (UserManager.IsInRole(id, "QuanTri"))
+                {
+                    return RedirectToAction("Default", "Default", new { area = "Admin" });
+                }
+                // nếu là Trực ban nhà trường
+                else if (UserManager.IsInRole(id, "TrucBan"))
+                {
+                    return RedirectToAction("Index", "BaoBanQs", new { area = "" });
+                }
+                // nếu là trực ban huấn luyện
+                else if (UserManager.IsInRole(id, "TrucBanHl"))
+                {
+                    return RedirectToAction("Index", "BaoBanHl", new { area = "" });
+                }
+                // còn lại 
+                else
+                {
+                    return RedirectToAction("Index", "Home", new { area = "" });
+                }
+
+            }
+            ViewBag.ReturnUrl = returnUrl ?? Url.Action("Index", "Home");
+            return View();
+        }
+
+        //
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // This doesn't count login failures towards account lockout
+            // To enable password failures to trigger account lockout, change to shouldLockout: true
+            var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, shouldLockout: false);
+            switch (result)
+            {
+                case SignInStatus.Success:
+                    //return RedirectToLocal(returnUrl);
+                    ApplicationUser user = await UserManager.FindAsync(model.UserName, model.Password);
+                    // nếu là trực ban đơn vị
+                    if (UserManager.IsInRole(user.Id, "DonVi"))
+                    {
+                        return RedirectToAction("Create", "BaoBanQs");
+                    }
+                    // nếu là Quan trị viên
+                    else if (UserManager.IsInRole(user.Id, "QuanTri"))
+                    {
+                        return RedirectToAction("Default", "Default", new { area = "Admin" });
+                    }
+                    // nếu là Trực ban nhà trường
+                    else if (UserManager.IsInRole(user.Id, "TrucBan"))
+                    {
+                        return RedirectToAction("Index", "BaoBanQs", new { area = "" });
+                    }
+                    // nếu là trực ban huấn luyện
+                    else if (UserManager.IsInRole(user.Id, "TrucBanHl"))
+                    {
+                        return RedirectToAction("Index", "BaoBanHl", new { area = "" });
+                    }
+                    // còn lại 
+                    else if (UserManager.IsInRole(user.Id, "NguoiDung"))
+                    {
+                        return RedirectToAction("Index", "Home", new { area = "" });
+                    }
+                    return View(model);
+                    //return RedirectToActionPermanent("Index", "Role", new { area = "Admin" });
+                case SignInStatus.LockedOut:
+                    return View("Lockout");
+                case SignInStatus.RequiresVerification:
+                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+                case SignInStatus.Failure:
+                default:
+                    ModelState.AddModelError("", "Sai tên đăng nhập hoặc mật khẩu.");
+                    return View();
+            }
+        }      
+        //
+        // GET: /Account/Register
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            ViewBag.Name = new SelectList(context.Roles.ToList(), "Name", "Name");
+            // lấy danh sách đơn vị gán vào List với value=Id, Text = TenDonVi
+            ViewBag.DonVi = new SelectList(db.DonVis.ToList(), "Id", "TenDonVi");
+            return View();
+        }
+
+        //
+        // POST: /Account/Register
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, MaDonVi = model.DonVi };
+                var result = await UserManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    //Assign Role to user Here 
+                    await this.UserManager.AddToRoleAsync(user.Id, model.Name);
+                    //Ends Here
+
+                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+                    
+                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+                    // Send an email with this link
+                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                    return RedirectToAction("Index", "Home");
+                }
+                AddErrors(result);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+        //
+        // POST: /Account/LogOff
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            return RedirectToAction("Login", "Account");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_userManager != null)
+                {
+                    _userManager.Dispose();
+                    _userManager = null;
+                }
+
+                if (_signInManager != null)
+                {
+                    _signInManager.Dispose();
+                    _signInManager = null;
+                }
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Helpers
+        // Used for XSRF protection when adding external logins
+        private const string XsrfKey = "XsrfId";
+
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        internal class ChallengeResult : HttpUnauthorizedResult
+        {
+            public ChallengeResult(string provider, string redirectUri)
+                : this(provider, redirectUri, null)
+            {
+            }
+
+            public ChallengeResult(string provider, string redirectUri, string userId)
+            {
+                LoginProvider = provider;
+                RedirectUri = redirectUri;
+                UserId = userId;
+            }
+
+            public string LoginProvider { get; set; }
+            public string RedirectUri { get; set; }
+            public string UserId { get; set; }
+
+            public override void ExecuteResult(ControllerContext context)
+            {
+                var properties = new AuthenticationProperties { RedirectUri = RedirectUri };
+                if (UserId != null)
+                {
+                    properties.Dictionary[XsrfKey] = UserId;
+                }
+                context.HttpContext.GetOwinContext().Authentication.Challenge(properties, LoginProvider);
+            }
+        }
+        #endregion
+    }
+}
