a19691f174f36edf89d38bd58a28d376ad9025a1
Dung Nguyen <soldiers1987@gmail.com>
2018-06-14 14:27:06 +0700

    add duy trung patient pages

--- Tm.Web/App_Start/RouteConfig.cs
+++ Tm.Web/App_Start/RouteConfig.cs
@@ -12,6 +12,8 @@ namespace TM.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            
+          
 
             routes.MapRoute(
                 name: "Default",
